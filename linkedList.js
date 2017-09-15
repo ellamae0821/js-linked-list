b76b0d5e4e009b225cf84d66a7f8704b221ab055
Ella Mae A <ellamae207@gmail.com>
2017-09-14 14:06:29 -1000

    passes 4

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,39 @@
  */
 function linkedListGenerator(){
 
+  var daHead = null;
+  var daTail = null;
+
+  function getHead() {
+    return daHead;
+  }
+
+  function getTail() {
+    return daTail;
+  }
+
+  function add (value){
+    var newNode = {
+      value: value,
+      next: null
+    };
+    if (getHead() === null){
+      daHead = newNode;
+      daTail = newNode;
+    }
+    return newNode;
+  }
+
+  function get (){
+
+  }
+
+  function remove (){
+
+  }
+
+  return{
+    getHead: getHead,
+    getTail: getTail
+  };
 }
